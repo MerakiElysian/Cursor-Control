b0f1c330f871ae146e460be98431c7b5c480eb64
Himanshu Dubey <himanshudubey666@gmail.com>
2025-11-23 22:25:28 +0530

    added mapper.py | seperate file for draw and added framerate

--- src/main.py
+++ src/main.py
@@ -1,26 +1,28 @@
 # main.py
 from Capture import Capture
 from Detector import Detector
+from Mapper import Mapper
 import cv2 as cv
 
 def main():
     cam = Capture(cam_index=0, mirror=True, rgb=True)
     detect = Detector()
+    mapper = Mapper()
 
     while True:
         frame_bgr, frame_rgb, success = cam.get_frame()
-
+        
         if not success:
             print("Failed to get frame")
             break
-
+        mapper._update_fps()
         # Send RGB frame to Mediapipe
         results = detect.detect(frame_rgb)
 
         # Draw landmarks (if detected)
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                detect.draw(frame_bgr, hand_landmarks)
+                mapper.draw(frame_bgr, hand_landmarks)
 
         # Show output
         cv.imshow("Hand Detection", frame_bgr)
